0c7c8e6de601a200d3783f1746c4d569e458581e
delerion <nelexeon@gmail.com>
2022-09-04 19:42:04 +0300

    nake snackBar for add link

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { SnackbarProvider, useSnackbar } from 'notistack'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <SnackbarProvider maxSnack={1} autoHideDuration={1000} >
     <App />
+    </SnackbarProvider>
 );
 
--- src/pages/table/table.jsx
+++ src/pages/table/table.jsx
@@ -2,82 +2,78 @@ import styled from "styled-components";
 import { useEffect, useRef } from "react";
 import { useState } from "react";
 
-import {TbCopy} from 'react-icons/tb';
-import {AiOutlineArrowUp} from 'react-icons/ai';
-import {AiOutlineArrowDown} from 'react-icons/ai';
+import { TbCopy } from "react-icons/tb";
+import { AiOutlineArrowUp } from "react-icons/ai";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
+import { SnackbarProvider, VariantType, useSnackbar } from "notistack";
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCellMui from '@mui/material/TableCell';
-import TableRowMui from '@mui/material/TableRow';
-import TableHead from '@mui/material/TableHead';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import Slide from '@mui/material/Slide';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCellMui from "@mui/material/TableCell";
+import TableRowMui from "@mui/material/TableRow";
+import TableHead from "@mui/material/TableHead";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Slide from "@mui/material/Slide";
 
 const TableRow = styled(TableRowMui)`
-background-color:#f0f1fc;
--webkit-box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.08);
--moz-box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.08);
-box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.08);
-
+  background-color: #f0f1fc;
+  -webkit-box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.08);
+  -moz-box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.08);
+  box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.08);
 `;
 
 const TableRowHeader = styled(TableRow)`
-  background-color:#b1b1b1;
-  cursor:pointer;
-
+  background-color: #b1b1b1;
+  cursor: pointer;
 `;
 
 const TableCellHeader = styled(TableCellMui)`
-  text-align:center !important;
-  cursor:pointer;
-  &:hover{
-    background-color:#c7bfbf;
-    transition:all 200ms;
+  text-align: center !important;
+  cursor: pointer;
+  &:hover {
+    background-color: #c7bfbf;
+    transition: all 200ms;
   }
 `;
 
 const TableCell = styled(TableCellMui)`
-  text-align:center !important;
+  text-align: center !important;
 `;
 
 const TableCellTarget = styled(TableCell)`
-  max-width:500px;
-  min-width:500px;
-  width:500px;
+  max-width: 500px;
+  min-width: 500px;
+  width: 500px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-`;             
+`;
 
 const TableBlock = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin:20px 0 20px 0;
+  margin: 20px 0 20px 0;
 `;
 
 const FormLink = styled.div`
-display:flex;
-gap:20px;
-margin-top:25px;
-`
+  display: flex;
+  gap: 20px;
+  margin-top: 25px;
+`;
 
 const CopyButton = styled(TbCopy)`
-margin-left:5px;
-cursor:pointer;
-`
+  margin-left: 5px;
+  cursor: pointer;
+`;
 
 const USERS_LIMIT = 10;
-
-
-
 
 const TableUs = () => {
   const [usersInfo, setUsersInfo] = useState([]);
@@ -88,9 +84,8 @@ const TableUs = () => {
   const allPage = Math.ceil(allPageUser / 10);
   const arrayPage = [];
   const [sortBy, setSortBy] = useState("asc_short");
-  const [lastCopiedText, setLastCopiedText] = useState('');
-  const [isLoad, setIsLoad] = useState(false)
-
+  const [lastCopiedText, setLastCopiedText] = useState("");
+  const [isLoad, setIsLoad] = useState(false);
 
   for (let i = 0; i < allPage; i++) {
     arrayPage.push(i);
@@ -102,7 +97,7 @@ const TableUs = () => {
   };
 
   async function getDataUsers() {
-    setIsLoad(prev=>!prev)
+    setIsLoad((prev) => !prev);
     await fetch(
       `http://79.143.31.216/statistics?order=${sortBy}&offset=${nextP}&limit=${USERS_LIMIT}`,
       {
@@ -116,14 +111,13 @@ const TableUs = () => {
       .then((res) => res.json())
       .then((data) => {
         setUsersInfo([...data]);
-        setIsLoad(prev=>!prev)
+        setIsLoad((prev) => !prev);
       });
   }
 
-
   useEffect(() => {
     getDataUsers();
-  }, [nextP,sortBy]);
+  }, [nextP, sortBy]);
 
   async function getCounterPage() {
     await fetch(`http://79.143.31.216/statistics?order=asc_short&offset=0`, {
@@ -139,9 +133,12 @@ const TableUs = () => {
       });
   }
   useEffect(() => {
-    
     getCounterPage();
   }, []);
+
+  const snackSendLink = (variant) => {
+    enqueueSnackbar("Ссылка создана", { variant });
+  };
 
   const send = () => {
     const linkValue = encodeURIComponent(link);
@@ -174,112 +171,97 @@ const TableUs = () => {
     sendLink();
     setlink("");
     getDataUsers();
-    getCounterPage()
+    getCounterPage();
+    snackSendLink("success");
   };
 
-const sortByShort = ()=>{
-    setSortBy(prev=>{
-        if(prev==='asc_short'){
-            return 'desc_short'
-        }
-        else if (prev==='desc_short'){
-            return 'asc_short'
-        }
-        else return 'asc_short'
-    })
-}
+  const sortByShort = () => {
+    setSortBy((prev) => {
+      if (prev === "asc_short") {
+        return "desc_short";
+      } else if (prev === "desc_short") {
+        return "asc_short";
+      } else return "asc_short";
+    });
+  };
 
-const sortByTarget = ()=>{
-    setSortBy(prev=>{
-        if(prev==='asc_target'){
-            return 'desc_target'
-        }
-        else if (prev==='desc_target'){
-            return 'asc_target'
-        }
-        else return 'asc_target'
-    })
-}
+  const sortByTarget = () => {
+    setSortBy((prev) => {
+      if (prev === "asc_target") {
+        return "desc_target";
+      } else if (prev === "desc_target") {
+        return "asc_target";
+      } else return "asc_target";
+    });
+  };
 
-const sortByCounter = ()=>{
-    setSortBy(prev=>{
-        if(prev==='asc_counter'){
-            return 'desc_counter'
-        }
-        else if (prev==='desc_counter'){
-            return 'asc_counter'
-        }
-        else return 'asc_counter'
-    })
-};
+  const sortByCounter = () => {
+    setSortBy((prev) => {
+      if (prev === "asc_counter") {
+        return "desc_counter";
+      } else if (prev === "desc_counter") {
+        return "asc_counter";
+      } else return "asc_counter";
+    });
+  };
+  const { enqueueSnackbar } = useSnackbar();
 
-  const copyLink = (link)=>{
+  const snackCopyText = () => {
+    enqueueSnackbar("Ссылка скопирована");
+  };
+
+  const copyLink = (link) => {
     setLastCopiedText(`http://79.143.31.216/s/${link}`);
-    openCopySnackbar();
-    navigator.clipboard.writeText(`http://79.143.31.216/s/${link}`)
-  }
-
-  const [isCopySnackbarOpen, setIsCopySnackbarOpen] = useState(false);
-
-  const openCopySnackbar = () => {
-    setIsCopySnackbarOpen(true);
+    snackCopyText();
+    navigator.clipboard.writeText(`http://79.143.31.216/s/${link}`);
   };
 
-  const closeCopySnackbar = () => {
-    setIsCopySnackbarOpen(false);
-  }
-
-  const TransitionLeft = (props) => {
-    return <Slide {...props} direction="left" />;
-  }
-     
   return (
     <TableBlock>
-
-<Table>
-  <TableHead>
-    <TableRowHeader>
-      <TableCellHeader onClick={sortByShort}>
-        Короткая ссылка
-      {sortBy === 'asc_short' && <AiOutlineArrowDown size={20}/>}
-      {sortBy === 'desc_short' && <AiOutlineArrowUp size={20}/>}
-      </TableCellHeader>
-      <TableCellHeader onClick={sortByTarget}>
-      {sortBy === 'asc_target' && <AiOutlineArrowDown size={20}/>}
-      {sortBy === 'desc_target' && <AiOutlineArrowUp size={20}/>}
-        Полная ссылка
-        </TableCellHeader>
-      <TableCellHeader onClick={sortByCounter}>
-      {sortBy === 'asc_counter' && <AiOutlineArrowDown size={20}/>}
-      {sortBy === 'desc_counter' && <AiOutlineArrowUp size={20}/>}
-        Количество переходов</TableCellHeader>
-    </TableRowHeader>
-  </TableHead>
-  <TableBody>
-    {usersInfo.map(user=><TableRow key={user.id}>
-      <TableCell>
-      {isLoad?'Загрузка':<a href={`http://79.143.31.216/s/${user.short}`}>
-                {user.short}
-              </a>}
-              <CopyButton size={20} onClick={()=>copyLink(user.short)}
->
-  Copy
-</CopyButton>
-
-      </TableCell>
-      <TableCellTarget>
-      {isLoad?'Загрузка':<a href={user.target}>
-                {user.target}
-              </a>}
-      </TableCellTarget>
-      <TableCell>
-        {isLoad?'Загрузка':user.counter}
-        
-      </TableCell>
-      </TableRow>)}
-  </TableBody>
-  <TableFooter>
-  <TableRow>
+      <Table>
+        <TableHead>
+          <TableRowHeader>
+            <TableCellHeader onClick={sortByShort}>
+              Короткая ссылка
+              {sortBy === "asc_short" && <AiOutlineArrowDown size={20} />}
+              {sortBy === "desc_short" && <AiOutlineArrowUp size={20} />}
+            </TableCellHeader>
+            <TableCellHeader onClick={sortByTarget}>
+              {sortBy === "asc_target" && <AiOutlineArrowDown size={20} />}
+              {sortBy === "desc_target" && <AiOutlineArrowUp size={20} />}
+              Полная ссылка
+            </TableCellHeader>
+            <TableCellHeader onClick={sortByCounter}>
+              {sortBy === "asc_counter" && <AiOutlineArrowDown size={20} />}
+              {sortBy === "desc_counter" && <AiOutlineArrowUp size={20} />}
+              Количество переходов
+            </TableCellHeader>
+          </TableRowHeader>
+        </TableHead>
+        <TableBody>
+          {usersInfo.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>
+                {isLoad ? (
+                  "Загрузка"
+                ) : (
+                  <a href={`http://79.143.31.216/s/${user.short}`}>
+                    {user.short}
+                  </a>
+                )}
+                <CopyButton size={20} onClick={() => copyLink(user.short)}>
+                  Copy
+                </CopyButton>
+              </TableCell>
+              <TableCellTarget>
+                {isLoad ? "Загрузка" : <a href={user.target}>{user.target}</a>}
+              </TableCellTarget>
+              <TableCell>{isLoad ? "Загрузка" : user.counter}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
             <TablePagination
               rowsPerPageOptions={[]}
               colSpan={3}
@@ -288,29 +270,27 @@ const sortByCounter = ()=>{
               page={currentPage}
               SelectProps={{
                 inputProps: {
-                  'aria-label': 'rows per page',
+                  "aria-label": "rows per page",
                 },
                 native: true,
               }}
               onPageChange={(e, page) => changePage(page)}
             />
           </TableRow>
-  </TableFooter>
-</Table>
+        </TableFooter>
+      </Table>
 
-<FormLink>
-<TextField  label="ссылка" variant="standard" value={link} onChange={(e) => setlink(e.target.value)} />
-<Button variant="contained" onClick={send}>Создать ссылку</Button>
-</FormLink>
-    <Snackbar
-        open={isCopySnackbarOpen}
-        onClose={closeCopySnackbar}
-        autoHideDuration={1500}
-        TransitionComponent={TransitionLeft}
-        message="Короткая ссылка скопирована"
-        key={lastCopiedText}
-      />
-      
+      <FormLink>
+        <TextField
+          label="ссылка"
+          variant="standard"
+          value={link}
+          onChange={(e) => setlink(e.target.value)}
+        />
+        <Button variant="contained" onClick={send}>
+          Создать ссылку
+        </Button>
+      </FormLink>
     </TableBlock>
   );
 };
